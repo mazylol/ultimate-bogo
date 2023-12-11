94e0426636911cf5e3d81f8773a7dab9f93053ce
Landon <landonp95@gmail.com>
2023-12-11 16:48:11 -0500

    on clang++

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-CC ?= g++
-
 default:
-	$(CC) -std=c++20 -static -O3 main.cpp -o main
+	clang++ -std=c++20 -static -O3 main.cpp -o main
 	./main
--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 #include "Bogo.h"
 
 int main() {
-    std::vector<int> vec = {3, 5, 1, 9, 6, 8};
+    /*std::vector<int> vec = {3, 5, 1, 9, 6, 8};
 
     Bogo::sort(&vec);
 
     for (auto &item : vec) {
         std::cout << item << " ";
     }
-    std::cout << "\n";
+    std::cout << "\n";*/
+
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }
