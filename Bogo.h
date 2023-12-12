9dfdc6d2be52c3b3718750ff88e9e4dd81287abf
Landon <landonp95@gmail.com>
2023-12-12 15:04:05 +0000

    old version was kind of working, unfortunately not fixable without breaking the bogosort ethos, now this is working the dumb way

--- Bogo.h
+++ Bogo.h
@@ -18,7 +18,14 @@ class Bogo {
     }
 
   public:
+    // lame version
     template <typename T>
+    static std::vector<T> sort(std::vector<T> vec) {
+        return sorting_fn(vec);
+    }
+
+    // multithreaded version
+    /*template <typename T>
     static std::vector<T> sort(std::vector<T> vec, int threads) {
         if (threads <= 1) {
             return sorting_fn(vec);
@@ -51,6 +58,34 @@ class Bogo {
 
         for (int i = 0; i < threads; i++) {
             vec.insert(vec.end(), sub_vecs[i].begin(), sub_vecs[i].end());
+        }
+
+        return vec;
+    }
+    */
+
+    // better multithreaded version
+    template <typename T>
+    static std::vector<T> sort(std::vector<T> vec, int threads) {
+        if (threads <= 1) {
+            return sorting_fn(vec);
+        }
+
+        std::vector<std::future<std::vector<T>>> futures;
+
+        for (int i = 0; i < threads; i++) {
+            futures.push_back(std::async(std::launch::async, sorting_fn<T>, vec));
+        }
+
+        for (int i = 0; i < threads; i++) {
+            futures[i].wait();
+        }
+
+        for (int i = 0; i < threads; i++) {
+            if (futures[i].wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+                vec = futures[i].get();
+                break;
+            }
         }
 
         return vec;
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "Bogo.h"
 
 int main() {
-    std::vector<int> vec = {3, 5, 1, 9, 6, 8, 7, 30, 2, 4, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+    std::vector<int> vec = {3, 5, 1, 9, 6, 8, 7, 30, 2, 4};
 
     vec = Bogo::sort(vec, 2);
 
