bd813f20ccd0d59ba8ccd9dc8acc140fcb3f1527
Landon <landonp95@gmail.com>
2024-01-13 12:25:11 -0500

    it actually works now

--- Bogo.h
+++ Bogo.h
@@ -1,90 +1,45 @@
 #include <algorithm>
 #include <chrono>
 #include <future>
-#include <memory>
+#include <iostream>
 #include <random>
-#include <thread>
 #include <vector>
 
 class Bogo {
     template <typename T>
-    static std::vector<T> sorting_fn(std::vector<T> vec) {
-        while (!std::is_sorted(vec.begin(), vec.end())) {
+    static std::vector<T> sorting_fn(std::vector<T> vec, std::atomic<bool> &success) {
+        while (!success.load() && !std::is_sorted(vec.begin(), vec.end())) {
             unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
             std::shuffle(vec.begin(), vec.end(), std::default_random_engine(seed));
         }
 
+        success.store(true);
         return vec;
     }
 
   public:
-    // lame version
-    template <typename T>
-    static std::vector<T> sort(std::vector<T> vec) {
-        return sorting_fn(vec);
-    }
-
-    // multithreaded version
-    /*template <typename T>
-    static std::vector<T> sort(std::vector<T> vec, int threads) {
-        if (threads <= 1) {
-            return sorting_fn(vec);
-        }
-
-        std::vector<std::future<std::vector<T>>> futures;
-        std::vector<std::vector<T>> sub_vecs;
-
-        for (int i = 0; i < threads; i++) {
-            sub_vecs.push_back(std::vector<T>());
-        }
-
-        for (int i = 0; i < vec.size(); i++) {
-            sub_vecs[i % threads].push_back(vec[i]);
-        }
-
-        for (int i = 0; i < threads; i++) {
-            futures.push_back(std::async(std::launch::async, sorting_fn<T>, sub_vecs[i]));
-        }
-
-        for (int i = 0; i < threads; i++) {
-            futures[i].wait();
-        }
-
-        for (int i = 0; i < threads; i++) {
-            sub_vecs[i] = futures[i].get();
-        }
-
-        vec.clear();
-
-        for (int i = 0; i < threads; i++) {
-            vec.insert(vec.end(), sub_vecs[i].begin(), sub_vecs[i].end());
-        }
-
-        return vec;
-    }
-    */
-
-    // better multithreaded version
     template <typename T>
     static std::vector<T> sort(std::vector<T> vec, int threads) {
         if (threads <= 1) {
-            return sorting_fn(vec);
+            std::atomic<bool> success(false);
+            return sorting_fn(vec, success);
         }
 
+        std::atomic<bool> success(false);
         std::vector<std::future<std::vector<T>>> futures;
 
         for (int i = 0; i < threads; i++) {
-            futures.push_back(std::async(std::launch::async, sorting_fn<T>, vec));
+            futures.push_back(std::async(
+                std::launch::async, [](std::vector<T> v, std::atomic<bool> &s) {
+                    return sorting_fn(v, s);
+                },
+                vec, std::ref(success)));
         }
 
-        for (int i = 0; i < threads; i++) {
-            futures[i].wait();
-        }
-
-        for (int i = 0; i < threads; i++) {
-            if (futures[i].wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
-                vec = futures[i].get();
-                break;
+        for (auto &future : futures) {
+            std::vector<T> sorted = future.get();
+            if (success.load() && std::is_sorted(sorted.begin(), sorted.end())) {
+                return sorted;
             }
         }
 
--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include "Bogo.h"
 
 int main() {
-    std::vector<int> vec = {3, 5, 1, 9, 6, 8, 7, 30, 2, 4};
+    std::vector<int> vec = {3, 5, 1, 9, 6, 8, 9, 27, 28};
 
-    vec = Bogo::sort(vec, 2);
+    vec = Bogo::sort(vec, 8);
 
     for (auto &item : vec) {
         std::cout << item << " ";
